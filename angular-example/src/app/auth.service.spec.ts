32468da0a20f33ced5969404098431a34ef64ada
Friso Denijs <friso1990@gmail.com>
2019-01-27 14:43:51 +0100

    added service tests and fixed broken tests

--- angular-example/src/app/auth.service.spec.ts
+++ angular-example/src/app/auth.service.spec.ts
@@ -9,4 +9,29 @@ describe('AuthService', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+  it('should return true when name and password are the same', () => {
+    const service: AuthService = TestBed.get(AuthService);
+    const result = service.login('admin', 'admin');
+    expect(result).toBe(true);
+  });
+
+  it('should return false when name and password are not the same', () => {
+    const service: AuthService = TestBed.get(AuthService);
+    const result = service.login('admin', 'nimda');
+    expect(result).toBe(false);
+  });
+
+  it('should return true when logged in', () => {
+    const service: AuthService = TestBed.get(AuthService);
+    service.login('admin', 'admin');
+    const result = service.isLoggedIn();
+    expect(result).toBe(true);
+  });
+
+  it('should return false when not logged in', () => {
+    const service: AuthService = TestBed.get(AuthService);
+    const result = service.isLoggedIn();
+    expect(result).toBe(false);
+  });
 });
