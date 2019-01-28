7f330a4834e9a5acb678f9fa1caf275657683411
Friso Denijs <friso1990@gmail.com>
2019-01-28 20:39:46 +0100

    added http test

--- angular-example/src/app/shared/services/asteroid.service.spec.ts
+++ angular-example/src/app/shared/services/asteroid.service.spec.ts
@@ -1,13 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AsteroidService } from './asteroid.service';
+import { HttpClient } from '@angular/common/http';
+import { AsteroidRequest } from '../models';
 
 describe('AsteroidService', () => {
-  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientTestingModule]}));
+  let asteroidService: AsteroidService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    asteroidService = TestBed.get(AsteroidService);
+  });
 
   it('should be created', () => {
-    const service: AsteroidService = TestBed.get(AsteroidService);
-    expect(service).toBeTruthy();
+    expect(asteroidService).toBeTruthy();
   });
+
+  it('should call NASA\'s asteroid API', inject([HttpClient], (http: HttpClient) => {
+    spyOn(http, 'get').and.callFake(() => {});
+    const asteroidRequest = new AsteroidRequest();
+    asteroidRequest.apiKey = 'apikey';
+    asteroidRequest.endDate = '1990-12-28';
+    asteroidRequest.startDate = '1990-11-28';
+    const expectedCall = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=1990-11-28&end_date=1990-12-28&api_key=apikey';
+
+    asteroidService.getAsteroids(asteroidRequest);
+
+    expect(http.get).toHaveBeenCalledWith(expectedCall);
+  }));
 });
