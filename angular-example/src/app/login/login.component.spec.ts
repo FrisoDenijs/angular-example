a25f319485dd0b5394e999590316b2096bb79d10
Friso Denijs <friso.denijs@capgemini.com>
2019-01-28 13:32:55 +0100

    added test for service call from login component

--- angular-example/src/app/login/login.component.spec.ts
+++ angular-example/src/app/login/login.component.spec.ts
@@ -2,15 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule],
+      providers: [ AuthService ]
     })
     .compileComponents();
   }));
@@ -19,9 +22,19 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authService = TestBed.get(AuthService);
+    spyOn(authService, 'login').and.returnValue(true);
+
+    component.loginForm.controls['userName'].setValue('admin');
+    component.loginForm.controls['password'].setValue('admin');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call authService.login when calling login method', () => {
+    component.login();
+    expect(authService.login).toHaveBeenCalled();
   });
 });
