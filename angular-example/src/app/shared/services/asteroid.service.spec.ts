3c6a973ed3b117b6fb46c8855763c1784ddd1a2f
Friso Denijs <friso.denijs@capgemini.com>
2019-01-29 14:16:33 +0100

    returning observable from http in asteroid service test

--- angular-example/src/app/shared/services/asteroid.service.spec.ts
+++ angular-example/src/app/shared/services/asteroid.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AsteroidService } from './asteroid.service';
 import { HttpClient } from '@angular/common/http';
 import { AsteroidRequest } from '../models';
+import { of } from 'rxjs';
 
 describe('AsteroidService', () => {
   let asteroidService: AsteroidService;
@@ -21,15 +22,17 @@ describe('AsteroidService', () => {
   });
 
   it('should call NASA\'s asteroid API', inject([HttpClient], (http: HttpClient) => {
-    spyOn(http, 'get').and.callFake(() => {});
+    const obj = of(new Object());
+    spyOn(http, 'get').and.returnValue(obj);
     const asteroidRequest = new AsteroidRequest();
     asteroidRequest.apiKey = 'apikey';
     asteroidRequest.endDate = '1990-12-28';
     asteroidRequest.startDate = '1990-11-28';
     const expectedCall = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=1990-11-28&end_date=1990-12-28&api_key=apikey';
 
-    asteroidService.getAsteroids(asteroidRequest);
+    const result = asteroidService.getAsteroids(asteroidRequest);
 
     expect(http.get).toHaveBeenCalledWith(expectedCall);
+    expect(result).toBe(obj);
   }));
 });
